f7d55dabf3d3af3f4f4828d8fefa0d103022073f
Brian Wu <brian.wu@stanford.edu>
2023-07-17 17:33:43 -0700

    fix typo in break statement

--- src/deck/drivers/src/lpsTwrTag.c
+++ src/deck/drivers/src/lpsTwrTag.c
@@ -370,7 +370,7 @@ static uint32_t twrTagOnEvent(dwDevice_t *dev, uwbEvent_t event)
       DEBUG_PRINT("RR: d = %f\n", (double)*state.distance);
 
       return rxcallback(dev);
-      brea
+      break;
     case eventPacketSent:
       // LOG STATE after a packet is sent
       // DEBUG_PRINT("PS: d = %f\n", (double)*state.distance);
@@ -385,7 +385,7 @@ static uint32_t twrTagOnEvent(dwDevice_t *dev, uwbEvent_t event)
     case eventTimeout:  // Comes back to timeout after each ranging attempt
       {
         // LOG STATE after a timeout event occurs
-        // DEBUG_PRINT("TE: d = %f\n", (double)*state.distance);
+        DEBUG_PRINT("TE: d = %f\n", (double)*state.distance);
 
         uint16_t rangingState = locoDeckGetRangingState();
         if (!ranging_complete && !lpp_transaction) {
